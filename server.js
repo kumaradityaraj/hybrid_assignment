3b1c560f80186088f3a76158ee09231baf8b1344
kumaradityaraj <sedulous.0007@gmail.com>
2022-08-23 10:23:21 +0530

    Setting up mongo db

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 //Here we have imported the required module
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
 
+// Congig
 dotenv.config({ path: "config/config.env" });
+
+// Connecting to database
+connectDatabase()
 
 // We are telling the server to listen on our particular port
 app.listen(process.env.PORT, () => {
